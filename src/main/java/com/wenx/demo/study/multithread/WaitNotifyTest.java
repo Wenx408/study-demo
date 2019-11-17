6e3d8f018c4c529b9f3de40b522a9390c9d93599
Wenx <jwx90312@163.com>
2019-11-17 23:04:02 +0800

    feat:Java学习笔记3-线程通信

---
+++ src/main/java/com/wenx/demo/study/multithread/WaitNotifyTest.java
@@ -0,0 +1,37 @@
+package com.wenx.demo.study.multithread;
+
+/**
+ * @Author: Wenx
+ * @Description:
+ * @Date: Created in 2019/11/11 21:41
+ * @Modified By：
+ */
+public class WaitNotifyTest {
+    public static Object lock = new Object();
+    public static Object baozi = null;
+
+    public static void main(String[] args) throws InterruptedException {
+        // 启动线程
+        new Thread(() -> {
+            synchronized (lock) {
+                while (baozi == null) { // 如果没包子，则进入等待
+                    try {
+                        System.out.println("没包子，坐等");
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            System.out.println("买到包子，走人");
+        }).start();
+        // 2秒之后，做了个包子
+        Thread.sleep(2000L);
+        baozi = new Object();
+        synchronized (lock) {
+            lock.notifyAll();
+            System.out.println("包子出锅了");
+        }
+    }
+
+}
