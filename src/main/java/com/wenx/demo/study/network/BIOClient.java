ee37938ffcfa790842d74a29eb6f0d15191dcbf2
Wenx <jwx90312@163.com>
2019-11-19 19:14:41 +0800

    feat:Java学习笔记13-BIO阻塞、NIO非阻塞网络编程

---
+++ src/main/java/com/wenx/demo/study/network/BIOClient.java
@@ -0,0 +1,67 @@
+package com.wenx.demo.study.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+/**
+ * @Author: Wenx
+ * @Description:
+ * @Date: Created in 2019/11/18 21:31
+ * @Modified By：
+ */
+public class BIOClient {
+    public static void main(String[] args) throws IOException {
+        // 客户端请求与本机在20480端口建立TCP连接
+        Socket client = new Socket("127.0.0.1", 20480);
+        client.setSoTimeout(600000);
+        System.out.println("Socket客户端-启动");
+        // 获取Socket的输入流，用来接收从服务端发送过来的数据
+        BufferedReader buf = new BufferedReader(new InputStreamReader(client.getInputStream(), "UTF-8"));
+        // 获取Socket的输出流，用来发送数据到服务端
+        PrintStream out = new PrintStream(client.getOutputStream(), false, "UTF-8");
+        // 获取键盘输入
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+
+        new Thread(() -> {
+            String msg;
+            // 接收从客户端发送过来的数据
+            try {
+                System.out.print("请输入信息：");
+                while ((msg = input.readLine()) != null) {
+                    out.println(msg);
+                    if (msg.length() == 0) {
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        try {
+            String echo;
+            // 从服务器端接收数据有个时间限制（系统自设，也可以自己设置），超过了这个时间，便会抛出该异常
+            while ((echo = buf.readLine()) != null) {
+                if (echo.length() == 0) {
+                    System.out.println("服务端连接关闭");
+                    break;
+                }
+                System.out.println(echo);
+                System.out.print("请输入信息：");
+            }
+        } catch (SocketTimeoutException e) {
+            System.out.println("Time out, No response");
+        }
+
+        input.close();
+        if (client != null) {
+            // 如果构造函数建立起了连接，则关闭套接字，如果没有建立起连接，自然不用关闭
+            client.close(); // 只关闭socket，其关联的输入输出流也会被关闭
+            System.out.println("Socket客户端-关闭");
+        }
+    }
+}
---
+++ src/main/java/com/wenx/demo/study/network/NIOClient.java
@@ -0,0 +1,51 @@
+package com.wenx.demo.study.network;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+/**
+ * @Author: Wenx
+ * @Description:
+ * @Date: Created in 2019/11/19 13:26
+ * @Modified By：
+ */
+public class NIOClient {
+    public static void main(String[] args) throws IOException {
+        // 客户端请求与本机在20480端口建立TCP连接
+        SocketChannel client = SocketChannel.open();
+        client.configureBlocking(false); // 设置为非阻塞模式
+        client.connect(new InetSocketAddress("127.0.0.1", 20480));
+        while (!client.finishConnect()) {
+            // 没连接上，则一直等待
+            Thread.yield();
+        }
+        System.out.println("Socket客户端-启动");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入：");
+        // 发送内容
+        String msg = scanner.nextLine();
+        ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+        while (buffer.hasRemaining()) {
+            client.write(buffer);
+        }
+
+        // 读取响应
+        System.out.println("收到服务端响应:");
+        ByteBuffer requestBuffer = ByteBuffer.allocate(1024);
+        while (client.isOpen() && client.read(requestBuffer) != -1) {
+            // 长连接情况下,需要手动判断数据有没有读取结束 (此处做一个简单的判断: 超过0字节就认为请求结束了)
+            if (requestBuffer.position() > 0) break;
+        }
+        requestBuffer.flip();
+        byte[] content = new byte[requestBuffer.limit()];
+        requestBuffer.get(content);
+        System.out.println(new String(content));
+
+        scanner.close();
+        client.close();
+    }
+}
